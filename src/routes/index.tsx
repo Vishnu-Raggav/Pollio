f1447bd44be2b962bfd6d15ba05cad7db8d44a29
Vishnu-Raggav <vishnuraggav15@gmail.com>
2025-10-28 10:55:20 +0530

    add logo and brand name to the home page header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import logo from "/logo.svg";
+
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return (
+    <div className="w-screen h-screen bg-off-white py-20 px-30">
+      <header className="w-full h-fit flex items-center">
+        <div className="flex items-center gap-6">
+          <img src={logo} className="size-7" />
+          <span className="font-righteous text-4xl">Pollio</span>
+        </div>
+      </header>
+    </div>
+  );
 }
