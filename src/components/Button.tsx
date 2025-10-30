31cebf94157da09df88035f8841129323dd16a54
Vishnu-Raggav <vishnuraggav15@gmail.com>
2025-10-30 11:47:11 +0530

    make Button component accept html button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,22 @@
 import cn from "@/utils/cn";
+import type React from "react";
 import type { JSX } from "react";
 
-interface ButtonProps {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   text: string;
   variant: "primary" | "secondary";
   children?: JSX.Element;
-}
+};
 
-export default function Button({ text, variant, children }: ButtonProps) {
+export default function Button({
+  text,
+  variant,
+  children,
+  ...props
+}: ButtonProps) {
   return (
-    <div
+    <button
+      {...props}
       className={cn(
         "w-fit h-fit select-none cursor-pointer flex items-center gap-2 rounded-md px-3 max-md:px-2 py-1 border-[1px] font-satoshi-medium",
         variant === "primary"
@@ -19,6 +26,6 @@ export default function Button({ text, variant, children }: ButtonProps) {
     >
       {children}
       <span className="text-lg max-md:text-sm">{text}</span>
-    </div>
+    </button>
   );
 }
