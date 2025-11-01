363f75a3c1e304886941b968cd5aad18459e22c3
Vishnu-Raggav <vishnuraggav15@gmail.com>
2025-11-01 23:47:05 +0530

    add size prop to Button component, available sizes are medium and small

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,12 +4,14 @@ import type { JSX } from "react";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   text: string;
+  size?: "medium" | "small";
   variant: "primary" | "secondary";
   children?: JSX.Element;
 };
 
 export default function Button({
   text,
+  size,
   variant,
   children,
   ...props
@@ -21,11 +23,19 @@ export default function Button({
         "w-fit h-fit select-none cursor-pointer flex items-center gap-2 rounded-md px-3 max-md:px-2 py-1 border-[1px] font-satoshi-medium",
         variant === "primary" && "bg-black border-black text-white",
         variant === "secondary" && "bg-white border-black/25 text-black",
-        "disabled disabled:opacity-50 disabled:cursor-not-allowed disabled:text-neutral-400"
+        "disabled disabled:opacity-50 disabled:cursor-not-allowed disabled:text-neutral-400",
+        size === "small" && "py-2 px-3 rounded-lg"
       )}
     >
       {children}
-      <span className="text-lg max-md:text-sm">{text}</span>
+      <span
+        className={cn(
+          "text-lg max-md:text-sm",
+          size === "small" && "text-base"
+        )}
+      >
+        {text}
+      </span>
     </button>
   );
 }
