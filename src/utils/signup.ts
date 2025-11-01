7b280c3d6a29d0681ad6a11a62fa2d70765a76c0
Vishnu-Raggav <vishnuraggav15@gmail.com>
2025-11-01 11:29:21 +0530

    remove email redirect (not working)

--- src/utils/signup.ts
+++ src/utils/signup.ts
@@ -6,9 +6,6 @@ export default async function signup({ email, password }: signupProps) {
   const { error } = await supabase.auth.signUp({
     email,
     password,
-    options: {
-      emailRedirectTo: window.origin + "/dashboard",
-    },
   });
 
   if (error) throw error;
