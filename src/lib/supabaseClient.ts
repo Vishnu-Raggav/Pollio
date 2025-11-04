938a478a903c739d0b61761248174fb5071ba678
Vishnu-Raggav <vishnuraggav15@gmail.com>
2025-11-04 09:40:01 +0530

    generate database types and integrate it to the supabase client

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { type Database } from "@/lib/types/database.types";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-const client = createClient(supabaseUrl, supabaseKey);
+const client = createClient<Database>(supabaseUrl, supabaseKey);
 export default client;
