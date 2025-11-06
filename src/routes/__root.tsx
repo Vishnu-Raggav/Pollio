2f46031e5273853b138033926798f524ac1b5c9f
Vishnu-Raggav <vishnuraggav15@gmail.com>
2025-11-06 14:16:01 +0530

    wrap application using QueryClientProvider to allow queryClient access to all routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,25 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { Toaster } from "sonner";
 import { useMediaQuery } from "react-responsive";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const Route = createRootRoute({
   component: RouteComponent,
 });
 
+const queryClient = new QueryClient();
+
 function RouteComponent() {
   const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Outlet />
       <Toaster
         richColors
         position={isDesktop ? "bottom-center" : "top-center"}
         visibleToasts={2}
       />
-    </>
+    </QueryClientProvider>
   );
 }
